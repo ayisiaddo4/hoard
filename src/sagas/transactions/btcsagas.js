06f395691a8e1abe1e153d6b4809a0a6a35b8afa
Zach Gibb <zachgibb@users.noreply.github.com>
2018-12-05 12:25:14 -0500

    Adjust btc transactions fetching to grab all transactions (#215)
    * pull btc page fetching into helper function
    
    * Add pageNum argument to btc fetchPage
    
    this is so we can get all of the available transactions in a btc wallet
    
    * remove unnecessary imports to transactions/btcsagas

--- src/sagas/transactions/btcsagas.js
+++ src/sagas/transactions/btcsagas.js
@@ -1,13 +1,10 @@
-import Bitcoin from 'bitcoinjs-lib';
 import {TRANSACTION_FOUND} from './constants';
-import { WALLET_IMPORT_SUCCESS, WALLET_TRACK_SYMBOL_SUCCESS } from "screens/Wallet/constants";
+import { WALLET_TRACK_SYMBOL_SUCCESS } from "screens/Wallet/constants";
 import { SYMBOL_BTC } from "containers/App/constants";
-import { fork, all, take, select, takeEvery, call, put } from "redux-saga/effects";
+import { fork, all, takeEvery, call, put } from "redux-saga/effects";
 import { TYPE_SEND, TYPE_REQUEST } from 'screens/SendRequest/constants';
 
-import {BTCNodeRequest} from 'screens/Wallet/WalletInstances/BtcWallet';
 import {timestampPriceApi} from './ethsagas';
-import TxDecoder from './btc-tx-decoder';
 import Config from 'react-native-config';
 
 import api from 'lib/api';
@@ -18,66 +15,75 @@ export default function* btcTransactionsSagaWatcher() {
   ]);
 }
 
+export function* fetchPage(action, pageNum) {
+  try {
+    const endpoint = `${Config.BTC_NODE_ENDPOINT}/txs?address=${action.payload.publicAddress}&pageNum=${pageNum}`;
+    const response = yield api.get(endpoint);
+
+    const nextPage = pageNum + 1;
+    if (nextPage < response.pagesTotal) {
+      yield fork(fetchPage, action, nextPage);
+    }
+
+    for (let transaction of response.txs) {
+      const inAddresses = transaction.vin.map(vin => vin.addr);
+      const wasSend = inAddresses.includes(action.payload.publicAddress);
+
+      let from;
+      let to;
+      let amount;
+
+      if (wasSend) {
+        from = action.payload.publicAddress;
+        const firstOtherVout = transaction
+          .vout
+          .find(vout => vout.scriptPubKey.addresses[0] !== action.payload.publicAddress);
+
+        if (firstOtherVout) {
+          to = firstOtherVout.scriptPubKey.addresses[0];
+          amount = Number(firstOtherVout.value);
+        } else { // for some reason this was a send to yourself, but we should still show it
+          to = action.payload.publicAddress;
+          amount = transaction.vout.reduce((total, vout) => total + Number(vout.value), 0);
+        }
+      } else {
+        from = inAddresses[0];
+        to = action.payload.publicAddress;
+        const firstMyVout = transaction
+              .vout
+              .find(vout => vout.scriptPubKey.addresses[0] === action.payload.publicAddress);
+        amount = Number(firstMyVout.value);
+      }
+
+      const price = yield call(timestampPriceApi, `?fsym=BTC&tsyms=USD&ts=${transaction.time}`);
+
+      yield put({
+        type: TRANSACTION_FOUND,
+        transaction: {
+          type: wasSend ? TYPE_SEND : TYPE_REQUEST,
+          date: transaction.time * 1000,
+          symbol: SYMBOL_BTC,
+          from,
+          to,
+          amount,
+          price: amount * price.BTC.USD,
+          fiatTrade: false,
+          details: {
+            ...transaction,
+            hash: transaction.txid
+          }
+        }
+      });
+    }
+  } catch(e) {
+    if (__DEV__) {
+      // eslint-disable-next-line no-console
+      console.log('An error occurred while fetching BTC transacions: ', e);
+    }
+  }
+}
 export function* fetchTransactions(action) {
   if (action.payload.symbol === SYMBOL_BTC) {
-    try {
-      const endpoint = `${Config.BTC_NODE_ENDPOINT}/txs?address=${action.payload.publicAddress}`;
-      const response = yield api.get(endpoint);
-
-      for (let transaction of response.txs) {
-        const inAddresses = transaction.vin.map(vin => vin.addr);
-        const wasSend = inAddresses.includes(action.payload.publicAddress);
-
-        let from;
-        let to;
-        let amount;
-
-        if (wasSend) {
-          from = action.payload.publicAddress;
-          const firstOtherVout = transaction
-            .vout
-            .find(vout => vout.scriptPubKey.addresses[0] !== action.payload.publicAddress);
-
-          if (firstOtherVout) {
-            to = firstOtherVout.scriptPubKey.addresses[0];
-            amount = Number(firstOtherVout.value);
-          } else { // for some reason this was a send to yourself, but we should still show it
-            to = action.payload.publicAddress;
-            amount = transaction.vout.reduce((total, vout) => total + Number(vout.value), 0);
-          }
-        } else {
-          from = inAddresses[0];
-          to = action.payload.publicAddress;
-          const firstMyVout = transaction
-                .vout
-                .find(vout => vout.scriptPubKey.addresses[0] === action.payload.publicAddress);
-          amount = Number(firstMyVout.value);
-        }
-
-        const price = yield call(timestampPriceApi, `?fsym=BTC&tsyms=USD&ts=${transaction.time}`);
-
-        yield put({
-          type: TRANSACTION_FOUND,
-          transaction: {
-            type: wasSend ? TYPE_SEND : TYPE_REQUEST,
-            date: transaction.time * 1000,
-            symbol: SYMBOL_BTC,
-            from,
-            to,
-            amount,
-            price: amount * price.BTC.USD,
-            fiatTrade: false,
-            details: {
-              ...transaction,
-              hash: transaction.txid
-            }
-          }
-        });
-      }
-    } catch(e) {
-      if (__DEV__) {
-        console.log('An error occurred while fetching BTC transacions: ', e);
-      }
-    }
+    yield call(fetchPage, action, 0);
   }
 }
