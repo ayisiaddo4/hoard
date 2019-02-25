eb872be6e12815071919fb04f1a3b2ff84080428
Eric Gibb <ericgibb@gmail.com>
2019-02-25 18:03:57 -0500

    [FIX] conditionally load sagas

--- src/rootSagas.js
+++ src/rootSagas.js
@@ -12,27 +12,35 @@ yield all([example(), ...otherSagas]);
 */
 
 import { all } from 'redux-saga/effects';
+import CONFIG from 'src/config';
 
 import settingsSagas from 'screens/Settings/sagas';
 import authSagas from 'sagas/authentication';
 import contactsSagas from 'sagas/contacts';
 import transactionSagas from 'sagas/transactions';
 import initSagas from 'sagas/init';
+import updatesSagas from 'sagas/updatesSagas';
 import walletSagas from 'screens/Wallet/sagas';
 import pricingSagas from 'sagas/pricing/saga';
 import kycStatusWatcher from 'screens/KYC/sagas';
 import notificationSagas from 'containers/Notifications/sagas';
 
+const rootSagas = [
+  initSagas(),
+  settingsSagas(),
+  authSagas(),
+  contactsSagas(),
+  walletSagas(),
+  pricingSagas(),
+  transactionSagas(),
+  kycStatusWatcher(),
+  notificationSagas(),
+];
+
+if (CONFIG.CODEPUSH_IS_ENABLED) {
+  rootSagas.push(updatesSagas());
+}
+
 export default function* rootSaga() {
-  yield all([
-    initSagas(),
-    settingsSagas(),
-    authSagas(),
-    contactsSagas(),
-    walletSagas(),
-    pricingSagas(),
-    transactionSagas(),
-    kycStatusWatcher(),
-    notificationSagas(),
-  ]);
+  yield all(rootSagas);
 }
